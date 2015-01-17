b444719dbc34e5b420ff6b208f970aef80bedfb9
Jaakko Lukkari <jaakko@applifier.com>
2015-01-17 18:16:29 +0200

    Split conditional expressions to multiple lines. Split attributes when over set limit (50 chars) for now.

--- test/formatters/tags-spec.js
+++ test/formatters/tags-spec.js
@@ -48,6 +48,30 @@ describe('Formatters - Tags', function() {
     lines[4].trim().should.equal('</div>');
   });
 
+  it('should not split tags with long text whitespace in the end', function () {
+    var test = '<div><div>Here comes the test </div></div>';
+
+    formatter.setOptions({});
+    var result = formatter.format(test);
+
+    var lines = result.split('\n');
+
+    lines[0].trim().should.equal('<div>');
+    lines[1].trim().should.equal('<div>Here comes the test </div>');
+    lines[2].trim().should.equal('</div>');
+
+    test = '<div><div> Here comes the test</div></div>';
+
+    formatter.setOptions({});
+    result = formatter.format(test);
+
+    lines = result.split('\n');
+
+    lines[0].trim().should.equal('<div>');
+    lines[1].trim().should.equal('<div> Here comes the test</div>');
+    lines[2].trim().should.equal('</div>');
+  });
+
   it('should not split tags with short text', function () {
     var test = '<div>        <div>Here</div></div>';
 
@@ -111,14 +135,44 @@ describe('Formatters - Tags', function() {
     lines[4].trim().should.equal('</div>');
   });
 
+  it('should not add line break after return', function () {
+    var test = 'function foo() {\nreturn <div><span /></div>;\n}';
+
+    formatter.setOptions({});
+    var result = formatter.format(test);
+    var lines = result.split('\n');
+
+    lines[0].trim().should.equal('function foo() {');
+    lines[1].trim().should.equal('return <div>');
+    lines[2].trim().should.equal('<span />');
+    lines[3].trim().should.equal('</div>;');
+    lines[4].trim().should.equal('}');
+  });
+
+  it('should add line breaks to ternary operations', function () {
+    var test = '<div>{true ? <div /> : <div />}</div>';
+
+    formatter.setOptions({});
+    var result = formatter.format(test);
+    var lines = result.split('\n');
+
+    lines[0].trim().should.equal('<div>');
+    lines[1].trim().should.equal('{true ?');
+    lines[2].trim().should.equal('<div />');
+    lines[3].trim().should.equal(':');
+    lines[4].trim().should.equal('<div />');
+    lines[5].trim().should.equal('}');
+    lines[6].trim().should.equal('</div>');
+  })
+
   it('should only format tags within range if range is specified', function () {
     var test = '<div><div><p><div /></p></div></div>';
     formatter.setOptions({
       range: [[1,10],[1,24]]
     });
 
-    result = formatter.format(test);
-    lines = result.split('\n');
+    var result = formatter.format(test);
+    var lines = result.split('\n');
 
     lines[0].trim().should.equal('<div><div>');
     lines[1].trim().should.equal('<p>');
